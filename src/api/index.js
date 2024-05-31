2e10d3caf021fa132ad5ba1dadc8e91608c0aa0a
chenaoyu <chen.aoyv@outlook.com>
2024-05-31 10:12:36 +0800

    地区请求头

--- src/api/index.js
+++ src/api/index.js
@@ -4,17 +4,25 @@ const request = axios.create({
     baseURL:import.meta.env.VITE_APP_BASE_API,
     timeout:30000
 })
-
+const DomainNames = {
+  TH: "thb.44dog.cc",
+  IDN: "idn.44dog.cc",
+  VND: "vnd.44dog.cc",
+  PH: "php.44dog.cc",
+  TH: "localhost",
+};
 request.interceptors.request.use(function (config) {
     let gwRequestFlag=config.params?.gw//是否为gw请求
     if(gwRequestFlag){
       config.baseURL=import.meta.env.VITE_GW_BASE_API
     }else{
-      const domain = new URL(window.location.origin).hostname; // 获取主机名部分 "php.44dog.cc"
-      const subdomain = domain.split('.')[0]; // 获取 "php"
-      ['php','vnd','thb','idn'].includes(subdomain)
-      ?config.headers['country'] = subdomain
-      :config.headers['country'] = 'thb'
+      const domain = window.location.hostname;
+      const subdomain = Object.entries(DomainNames).find(([key, value]) => domain === value)?.[0];
+      config.headers['country'] = subdomain
+      // const subdomain = domain.split('.')[0]; // 获取 "php"
+      // ['php','vnd','thb','idn'].includes(subdomain)
+      // ?config.headers['country'] = subdomain
+      // :config.headers['country'] = 'thb'
     }
     return config;
   }, function (error) {

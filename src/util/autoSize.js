c3ebd9fe3a05ff371557a9bb4380d141a7265f54
chenaoyu <chen.aoyv@outlook.com>
2024-08-19 17:55:26 +0800

    测试

---
+++ src/util/autoSize.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('#body')
+const html = document.documentElement
+function setFontSize() {
+  var viewportWidth = window.innerWidth;
+  var fontSize = viewportWidth / 10;
+  document.documentElement.style.fontSize = fontSize + 'px';
+}
+function setStyle() {
+  body.style.width = '375px'
+  html.style.fontSize = '37.5px'
+}
+const judgeIsMobileDevice = () => /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+function handleResize() {
+  if (judgeIsMobileDevice()) {
+    body.style = ''
+    setFontSize()
+  } else {
+    setStyle()
+  }
+}
+window.addEventListener('resize', handleResize)
+handleResize();
+console.log('autoSize')
